069296155e30a3dbaf0e96e18da02b7a6e308aee
Alastair Jones <alastairjones@users.noreply.github.com>
2023-03-02 22:33:33 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
-import React,{useState}from 'react'
+import React from 'react'
+import {BrowserRouter as Router,Link,Route} from 'react-router-dom'
+import User from './User'
+
+//https://www.youtube.com/watch?v=unRB8FNCekM
+
 function App() {
-  const [data,setData]=useState("Anil")
+  let user=[
+    {name:'bob', id:1},
+    {name:'peter', id:2},
+    {name:'bruce',id:3},
+    {name:'tony',id:4}
+  ]
   return (
     <div className="App">
-      <h1>Hooks in React {data}</h1>
-      <button onClick={()=>setData("Peter")}>Update State</button>
-    </div>
+      <Router>
+      <h1>React router</h1>
+      {
+        user.map((item)=>
+        <div><Link key={item.id} to={"/user/"+item.id+"/"+item.name}>{item.name}</Link></div>
+        )
+      }
+      
+      </Router>
+      </div>
   );
 }
 
